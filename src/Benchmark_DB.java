d8bd152d5e79e8de3bf0737b18a0280c67487233
MazzenYo <56998097+MazzenYo@users.noreply.github.com>
2021-11-23 12:55:48 +0100

    Push zu branches

--- src/Benchmark_DB.java
+++ src/Benchmark_DB.java
@@ -10,23 +10,18 @@ public class Benchmark_DB {
         int n = scan.nextInt();
         scan.close();
         Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchmark-datenbank", "dbi", "dbi_pass");
-        assert false;
         Statement stmt = conn.createStatement();
         try
         {
 
             System.out.println("\nConnected to benchmark database!\n");
-            int balance = 0;
-            String branchnameString = "ABCDEFGHIJKLMNOPQRST";
-            String address = "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUV";
             for (int i = 1; i <= n; i++)
             {
-
-                String sqlFillBranches=
-                        ("insert into `benchmark-datenbank`.branches(branchid, branchname, balance, address) values(i,'branch' , '0', 'aaa' )");
-                stmt.executeUpdate(sqlFillBranches);
+                String sqlQuery = "insert into `benchmark-datenbank`.branches(branchid, branchname, balance, address) values ("+ i + ",'ABCDEFGHIJKLMNOPQRST' ,0,'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUV' ) ";
+                stmt.executeUpdate(sqlQuery);
                 System.out.println("Updates Branches");
             }
+
             stmt.close();
             conn.close();
             System.out.println("\nDisconnected!\n");
