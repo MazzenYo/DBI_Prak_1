2877a6f7ddfe96f11b1e23f70180404a23a16055
MazzenYo <56998097+MazzenYo@users.noreply.github.com>
2021-11-29 22:32:17 +0100

    optimized code mit entfernen von print anweisung in zeitmessung sowie umstrukturierung

--- src/Benchmark_DB.java
+++ src/Benchmark_DB.java
@@ -7,34 +7,20 @@ public class Benchmark_DB {
     public static void main(String[] args) throws SQLException
     {
         // Scanner für N Wert über Tastatur
-        long start = System.currentTimeMillis();
-
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter n:");
         int n = scan.nextInt();
         scan.close();
-        /*rewriteBatchedStatements=true*/
+
         Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchmark-datenbank?rewriteBatchedStatements=true&useServerPrepStmts=false", "dbi", "dbi_pass");
-        System.out.println("\nConnected to benchmark database!\n");
-        conn.setAutoCommit(false);
-        long start = System.currentTimeMillis();
-        try
-        {
-            String SQL_Insert_branches = "insert into `benchmark-datenbank`.branches(branchid, branchname, balance, address) values (?,?,?,?)";
-            PreparedStatement preparedStatement= conn.prepareStatement(SQL_Insert_branches);
-
-        final long timeStart = System.currentTimeMillis() / 1000;
-        //Verbindungsaufbau
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchmark-datenbank", "dbi", "dbi_pass");
-        Statement stmt = conn.createStatement();
-        try
-        {
-
+        try (conn) {
             System.out.println("\nConnected to benchmark database!\n");
+            conn.setAutoCommit(false);
+            long start = System.currentTimeMillis();
+            String SQL_Insert_branches = "insert into `benchmark-datenbank`.branches(branchid, branchname, balance, address) values (?,?,?,?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(SQL_Insert_branches);
 
-            for (int i = 1; i <= n; i++)
-            {
+            for (int i = 1; i <= n; i++) {
                 preparedStatement.setInt(1, i);
                 preparedStatement.setString(2, "ABCDEFGHIJKLMNOPQRST");
                 preparedStatement.setInt(3, 0);
@@ -42,10 +28,10 @@ public class Benchmark_DB {
                 preparedStatement.executeUpdate();
             }
             String SQL_Insert_accounts = "insert into `benchmark-datenbank`.accounts(accid, balance, branchid, name, address) values (?,?,?,?,?)";
-            preparedStatement= conn.prepareStatement(SQL_Insert_accounts);
-            for(int i = 1; i <= n * 100000; ++i) {
+            preparedStatement = conn.prepareStatement(SQL_Insert_accounts);
+            for (int i = 1; i <= n * 100000; ++i) {
                 Random rand = new Random();
-                int randomBranchid= rand.nextInt(n)+1;
+                int randomBranchid = rand.nextInt(n) + 1;
                 preparedStatement.setInt(1, i);
                 preparedStatement.setInt(2, 0);
                 preparedStatement.setInt(3, randomBranchid);
@@ -57,10 +43,10 @@ public class Benchmark_DB {
 
 
             String SQL_Insert_tellers = "insert into `benchmark-datenbank`.tellers(tellerid, balance, branchid, tellername, address) values (?,?,?,?,?) ";
-            preparedStatement= conn.prepareStatement(SQL_Insert_tellers);
-            for(int i = 1; i <= n * 10; ++i) {
+            preparedStatement = conn.prepareStatement(SQL_Insert_tellers);
+            for (int i = 1; i <= n * 10; ++i) {
                 Random rand = new Random();
-                int randomBranchid= rand.nextInt(n)+1;
+                int randomBranchid = rand.nextInt(n) + 1;
                 preparedStatement.setInt(1, i);
                 preparedStatement.setInt(2, 0);
                 preparedStatement.setInt(3, randomBranchid);
@@ -70,21 +56,14 @@ public class Benchmark_DB {
             }
             conn.commit();
             preparedStatement.close();
-            System.out.println("\nDisconnected!\n");
-        }
-        catch (SQLException e)
-        {
-            System.err.println(e.toString());
-            System.exit(1);
-        }
-        finally // close used resources
-        {
 
             long finish = System.currentTimeMillis();
             long timeElapsed = (finish - start) / 1000L;
-            String msg = "time needed n = "+ n + ": "+ timeElapsed+"  seconds";
+            String msg = "time needed n = " + n + ": " + timeElapsed + "  seconds";
             System.out.println(msg);
-            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.toString());
+            System.exit(1);
         }
     }
 }
