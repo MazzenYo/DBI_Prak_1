595ab4213883944d4ed75c1e4ccc215a5e8dd5e0
Mazzenyo <56998097+mazzenyo@users.noreply.github.com>
2021-12-02 23:19:13 +0100

    Kommentare hinzugefügt
    Signed-off-by: Mazzenyo <56998097+mazzenyo@users.noreply.github.com>

--- src/Benchmark_DB.java
+++ src/Benchmark_DB.java
@@ -2,28 +2,41 @@ import java.sql.*;
 import java.util.Scanner;
 import java.util.Random;
 
+/**
+ * The type Benchmark db.
+ */
 public class Benchmark_DB {
 
-    public static void main(String[] args) throws SQLException
-    {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws SQLException the sql exception
+     */
+    public static void main(String[] args) throws SQLException {
 
-/**/
+        //scanner für die Eingabe des N-Wertes
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter n:");
         int n = scan.nextInt();
         scan.close();
 
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchmark-datenbank?rewriteBatchedStatements=true&useServerPrepStmts=false", "dbi", "dbi_pass");
+        //Verbindungsaufbau über jdbc-Treiber
+        //übergebe der Verbindung die Servereinstellung, dass er BatchedStatements überschreiben darf
+        //verhindert die erneute Erstellung des servers von prepared Statements, da wir schon prepared Statements übergeben
+        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchdb?rewriteBatchedStatements=true&useServerPrepStmts=false", "dbi", "dbi_pass");
         System.out.println("\nSuccessfully connected to benchmark database!\n");
         conn.setAutoCommit(false);
         System.out.println("Starting time measurement!\n");
         long start = System.currentTimeMillis();
-        try
-        {
-            String SQL_Insert_branches = "insert into `benchmark-datenbank`.branches(branchid, branchname, balance, address) values (?,?,?,?)";
-            PreparedStatement preparedStatement= conn.prepareStatement(SQL_Insert_branches);
-            for (int i = 1; i <= n; i++)
-            {
+        PreparedStatement preparedStatement = null;
+
+        try {
+            //erzeuge String der in dem prepared Statement übernommen wird
+            String SQL_Insert_branches = "insert into `benchdb`.branches(branchid, branchname, balance, address) values (?,?,?,?)";
+            preparedStatement = conn.prepareStatement(SQL_Insert_branches);
+            //schleife die die preparedStatements mit den Werten ergänzt
+            for (int i = 1; i <= n; i++) {
                 preparedStatement.setInt(1, i);
                 preparedStatement.setString(2, "ABCDEFGHIJKLMNOPQRST");
                 preparedStatement.setInt(3, 0);
@@ -31,22 +44,25 @@ public class Benchmark_DB {
                 preparedStatement.executeUpdate();
             }
 
-            String SQL_Insert_accounts = "insert into `benchmark-datenbank`.accounts(accid, balance, branchid, name, address) values (?,?,?,?,?)";
+            String SQL_Insert_accounts = "insert into `benchdb`.accounts(accid, balance, branchid, name, address) values (?,?,?,?,?)";
             preparedStatement= conn.prepareStatement(SQL_Insert_accounts);
             for(int i = 1; i <= n * 100000; ++i) {
                 Random rand = new Random();
-                int randomBranchid= rand.nextInt(n)+1;
+                int randomBranchid = rand.nextInt(n) + 1;
                 preparedStatement.setInt(1, i);
                 preparedStatement.setInt(2, 0);
                 preparedStatement.setInt(3, randomBranchid);
                 preparedStatement.setString(4, "ABCDEFGHIJKLMNOPQRST");
                 preparedStatement.setString(5, "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQR");
+
+                //füge das prepared Statement zu einem Batch hinzu
                 preparedStatement.addBatch();
             }
+            //füge den Batch der Datenbank hinzu
             preparedStatement.executeBatch();
 
 
-            String SQL_Insert_tellers = "insert into `benchmark-datenbank`.tellers(tellerid, balance, branchid, tellername, address) values (?,?,?,?,?) ";
+            String SQL_Insert_tellers = "insert into `benchdb`.tellers(tellerid, balance, branchid, tellername, address) values (?,?,?,?,?) ";
             preparedStatement= conn.prepareStatement(SQL_Insert_tellers);
             for(int i = 1; i <= n * 10; ++i) {
                 Random rand = new Random();
@@ -58,23 +74,22 @@ public class Benchmark_DB {
                 preparedStatement.setString(5, "ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQR");
                 preparedStatement.executeUpdate();
             }
+            //Committe alle changes zur Connection
             conn.commit();
-            preparedStatement.close();
+
             System.out.println("\nDisconnected!\n");
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.err.println(e.toString());
             System.exit(1);
-        }
-        finally // close used resources
+        } finally // close used resources
         {
-
+            preparedStatement.close();
+            conn.close();
+            //beende die Zeitmessung
             long finish = System.currentTimeMillis();
             long timeElapsed = (finish - start) / 1000L;
-            String msg = "Ended time measurement: \n Time needed for n = "+ n + ": "+ timeElapsed+"  seconds";
+            String msg = "Ended time measurement: \n Time needed for n = " + n + ": " + timeElapsed + "  seconds";
             System.out.println(msg);
-            conn.close();
         }
     }
 }
--- src/initDatabase.java
+++ src/initDatabase.java
@@ -1,93 +1,97 @@
 import java.sql.*;
 
+/**
+ * The type Init database.
+ */
 public class initDatabase {
-    public static void main(String[] args) throws SQLException
-    {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/benchmark-datenbank", "dbi", "dbi_pass");
-        assert false;
-        Statement stmt = conn.createStatement();
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 * @throws SQLException the sql exception
+	 */
+	public static void main(String[] args) throws SQLException {
+		//baue Verbindung zur Datenbank auf
+		Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306", "dbi", "dbi_pass");
 
-        try
-        {
-            //conn.setAutoCommit(false);
-            System.out.println("\nConnected to benchmark database!\n");
+		//erzeuge Statements für die oben verbundene Datenbank
+		try (conn; Statement stmt = conn.createStatement()) {
 
-            String sqlDropTable=
-                    "DROP TABLE `benchmark-datenbank`.branches ,`benchmark-datenbank`.accounts ,`benchmark-datenbank`.history, `benchmark-datenbank`.tellers;"
-                    ;
-            stmt.executeUpdate(sqlDropTable);
-            System.out.println("\nDrop Tables\n");
+			//wenn das schema 'benchdb' schon existiert wird es gedroppt
+			String sqlDropSchemaIfExists = "DROP schema if exists benchdb;";
+			stmt.executeUpdate(sqlDropSchemaIfExists);
+			System.out.println("\nDrop schema if exists\n");
 
-            String sqlBranches=
-                    "create table branches\n" +
-                    "( branchid int not null,\n" +
-                    " branchname char(20) not null,\n" +
-                    " balance int not null,\n" +
-                    " address char(72) not null,\n" +
-                    " primary key (branchid) );"
-                    ;
-            stmt.executeUpdate(sqlBranches);
-            System.out.println("\nCreated Table branches\n");
+			//erzeuge das schema benchdb
+			String sqlCreateSchema = "create schema benchdb;";
+			stmt.executeUpdate(sqlCreateSchema);
+			System.out.println("\nCreated schema\n");
 
+			System.out.println("\nConnected to benchmark database!\n");
 
-            String sqlAccounts =
-                    "create table accounts\n" +
-                            "( accid int not null,\n" +
-                            " name char(20) not null,\n" +
-                            " balance int not null,\n" +
-                            "branchid int not null,\n" +
-                            "address char(68) not null,\n" +
-                            "primary key (accid),\n" +
-                            "foreign key (branchid) references `benchmark-datenbank`.branches(branchid) );"
-                    ;
-                stmt.executeUpdate(sqlAccounts);
-                System.out.println("\nCreated Table accounts\n");
+			//erzeuge Tabelle 'branches' in dem schema 'benchdb'
+			String sqlBranches =
+					"create table `benchdb`.branches\n" +
+							"( branchid int not null,\n" +
+							" branchname char(20) not null,\n" +
+							" balance int not null,\n" +
+							" address char(72) not null,\n" +
+							" primary key (branchid) );";
+			stmt.executeUpdate(sqlBranches);
+			System.out.println("\nCreated Table branches\n");
 
 
-            String sqlTellers =
-                    "create table tellers\n" +
-                            "( tellerid int not null,\n" +
-                            " tellername char(20) not null,\n" +
-                            " balance int not null,\n" +
-                            " branchid int not null,\n" +
-                            " address char(68) not null,\n" +
-                            " primary key (tellerid),\n" +
-                            " foreign key (branchid) references `benchmark-datenbank`.branches(branchid) ); "
-                    ;
-            stmt.executeUpdate(sqlTellers);
-            System.out.println("\nCreated Table tellers\n");
+			//erzeuge Tabelle 'accounts' in dem schema 'benchdb'
+
+			String sqlAccounts =
+					"create table `benchdb`.accounts\n" +
+							"( accid int not null,\n" +
+							" name char(20) not null,\n" +
+							" balance int not null,\n" +
+							"branchid int not null,\n" +
+							"address char(68) not null,\n" +
+							"primary key (accid),\n" +
+							"foreign key (branchid) references `benchdb`.branches(branchid) );";
+			stmt.executeUpdate(sqlAccounts);
+			System.out.println("\nCreated Table accounts\n");
+
+			//erzeuge Tabelle 'tellers' in dem schema 'benchdb'
+			String sqlTellers =
+					"create table `benchdb`.tellers\n" +
+							"( tellerid int not null,\n" +
+							" tellername char(20) not null,\n" +
+							" balance int not null,\n" +
+							" branchid int not null,\n" +
+							" address char(68) not null,\n" +
+							" primary key (tellerid),\n" +
+							" foreign key (branchid) references `benchdb`.branches(branchid) ); ";
+			stmt.executeUpdate(sqlTellers);
+			System.out.println("\nCreated Table tellers\n");
+
+			//erzeuge Tabelle 'history' in dem schema 'benchdb'
+
+			String sqlHistory =
+					"create table `benchdb`.history\n" +
+							"( accid int not null,\n" +
+							" tellerid int not null,\n" +
+							" delta int not null,\n" +
+							" branchid int not null,\n" +
+							" accbalance int not null,\n" +
+							" cmmnt char(30) not null,\n" +
+							" foreign key (accid) references `benchdb`.accounts(accid),\n" +
+							" foreign key (tellerid) references `benchdb`.tellers(tellerid),\n" +
+							" foreign key (branchid) references `benchdb`.branches(branchid) );";
+			stmt.executeUpdate(sqlHistory);
+			System.out.println("\nCreated Table history\n");
 
 
-
-            String sqlHistory =
-                    "create table history\n" +
-                            "( accid int not null,\n" +
-                            " tellerid int not null,\n" +
-                            " delta int not null,\n" +
-                            " branchid int not null,\n" +
-                            " accbalance int not null,\n" +
-                            " cmmnt char(30) not null,\n" +
-                            " foreign key (accid) references `benchmark-datenbank`.accounts(accid),\n" +
-                            " foreign key (tellerid) references `benchmark-datenbank`.tellers(tellerid),\n" +
-                            " foreign key (branchid) references `benchmark-datenbank`.branches(branchid) );"
-                    ;
-            stmt.executeUpdate(sqlHistory);
-            System.out.println("\nCreated Table history\n");
-
-            conn.close();
+			conn.close();  //close connection
 
             System.out.println("\nDisconnected!\n");
 
-        }
-        catch (SQLException e)
-        {
-            System.err.println(e.toString());
-            System.exit(1);
-        }
-        finally // close used resources
-        {
-            if (stmt!=null) stmt.close();
-            if (conn!=null) conn.close();
-        }
-    }
+		} catch (SQLException e) {
+			System.err.println(e.toString());
+			System.exit(1);
+		}
+	}
 }
